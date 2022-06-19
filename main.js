468b9101f7d2e5253343c6e107b35546c9b4f641
ketah <onyangioketta@gmail.com>
2022-06-19 15:41:02 +0300

    add

--- main.js
+++ main.js
@@ -1,36 +1,67 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+//const EMPTY_HEART = '♡'
+//const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-const heartResponce = document.querySelectorAll(".like-glyph");
+//const articleHearts = document.querySelectorAll(".like-glyph");
 
-function likeCallback(e) {
-  const heart = e.target;
-  mimicServerCall()
-    .then(function(){
-    if ( likeHeart.innerText === EMPTY_HEART)
-{
-  likeHeart.className = "activated-heart";
-}else {
-  likeHeart.innerText = EMPTY_HEART;
-  likeHeart.className = "";}
-
-
-
-    })
+//function likeCallback(e) {
+  //const heart = e.target;
+  //mimicServerCall()
   
-    .catch((error) =>{
-      const modal = document.getElementsById('modal')
-      modal.textContent=error;
+  //.then(function(){
+    //if(heart.textContent === EMPTY_HEART){
+    //heart.textContent = FULL_HEART;
+    //heart.className = "activated-heart";
+  //}else {
+    //heart.textContent = EMPTY_HEART;
+    //heart.className = "";
+  //};
+     
+  //})
+
+    //.catch(function(error) {
+      //const modal = document.getElementsById("modal")
+      //modal.className = "";
+      //modal.textContent=error;
+      //setTimeout(() =>  modal.className = "hidden", 3000);
+    //});
+//} 
+ //for (const glyph of articleHearts) {
+  //glyph.addEventListener("click", likeCallback);
+ //}
+
+ const EMPTY_HEART = '♡'
+ const FULL_HEART = '♥'
+ 
+ const articleHearts = document.querySelectorAll(".like-glyph");
+ 
+ function likeCallback(e) {
+   const heart = e.target;
+   mimicServerCall()
+     .then(function(){
+       if ( liker.innerText === EMPTY_HEART) {
+         liker.innerText = FULL_HEART;
+         liker.className = "activated-heart";
+       } else {
+         liker.innerText = EMPTY_HEART;
+         liker.className = "";
+       }
+     })
+     .catch((error)=> {
+      let modal = document.getElementById("modal");
+      modal.className = "";
+      modal.innerText = error;
       setTimeout(() =>  modal.className = "hidden", 3000);
     });
-} 
- for (const glyph of articleHearts) {
-  glyph.addEventListener("click", likeHeart);
+     
  }
-
-
+ 
+ for (const glyph of articleHearts) {
+   glyph.addEventListener("click", likeCallback);
+ }
+ 
+ 
 
 
 //------------------------------------------------------------------------------
@@ -39,7 +70,7 @@ function likeCallback(e) {
 
 function mimicServerCall(url="http://mimicServer.example.com", config={}) {
   return new Promise(function(resolve, reject) {
-    setTimeout(function() {
+   setTimeout(function() {
       let isRandomFailure = Math.random() < .2
       if (isRandomFailure) {
         reject("Random server error. Try again.");
